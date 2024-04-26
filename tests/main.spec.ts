bc81395744a5d6820645416de68a521e48d2ec3d
zavx0z <metaversebdfl@gmail.com>
2024-04-26 21:54:15 +0300

    тестирование

---
+++ tests/main.spec.ts
@@ -0,0 +1,26 @@
+import { describe, it, expect } from "bun:test"
+
+import { Cell, toNano } from "ton-core"
+import { hex } from "../build/main.compiled.json"
+import { Blockchain } from "@ton-community/sandbox"
+import { MainContract } from "../wrappers/MainContracts"
+import "@ton-community/test-utils"
+
+describe("main.fc contract tests", () => {
+  it("should get the proper most recent sender address", async () => {
+    const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0]
+    const blockchain = await Blockchain.create()
+    const myContract = blockchain.openContract(await MainContract.createFromConfig({}, codeCell))
+    const senderWallet = await blockchain.treasury("sender")
+    const sendMessageResult = await myContract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"))
+
+    //@ts-ignore
+    expect(sendMessageResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: true,
+    })
+    const data = await myContract.getData()
+    expect(data.resent_sender.toString()).toBe(senderWallet.address.toString())
+  })
+})
