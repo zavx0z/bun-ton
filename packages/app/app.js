16484cd03f53acd3c1b00733c4054f83ab9b14c9
zavx0z <metaversebdfl@gmail.com>
2024-05-01 23:23:29 +0300

    vite app

--- packages/app/app.js
+++ packages/app/app.js
@@ -1,9 +1,11 @@
-const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-  manifestUrl: "tonconnect-manifest.json",
+import { TonConnectUI } from "@tonconnect/ui"
+
+const tonConnectUI = new TonConnectUI({
+  manifestUrl: "https://zavx0z.github.io/bun-ton/tonconnect-manifest.json",
   buttonRootId: "ton-connect",
 })
 async function connectToWallet() {
-  const connectedWallet = await tonConnectUI.connectWallet()
+  const connectedWallet = await tonConnectUI.openModal()
   // Do something with connectedWallet if needed
   console.log(connectedWallet)
 }
