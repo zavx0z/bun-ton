48a5fef6cd1ec16767b94972a4180430dd2e40e2
zavx0z <metaversebdfl@gmail.com>
2024-05-02 18:25:15 +0300

    считывание данных из контракта

--- packages/app/src/app.ts
+++ packages/app/src/app.ts
@@ -1,4 +1,26 @@
 import { TonConnectUI } from "@tonconnect/ui"
+import { getHttpEndpoint } from "@orbs-network/ton-access"
+import { TonClient } from "@ton/ton"
+import { Address, OpenedContract } from "@ton/core"
+import { MainContract } from "contract/wrappers/MainContract"
+
+const createMainContract = async () => {
+  const endpoint = await getHttpEndpoint({ network: "testnet" })
+  const tonClient = new TonClient({ endpoint })
+  if (!tonClient) {
+    new Error("Failed to create TonClient")
+  }
+  const contract = new MainContract(Address.parse("EQCSh89SFOAtwK20gGzZ3TfFpsSwLvYj9qfRMJ55MaPRf4gB"))
+  return tonClient.open(contract) as OpenedContract<MainContract>
+}
+
+createMainContract().then(async (mainContract) => {
+  document.getElementById("address")!.innerHTML = mainContract.address.toString().slice(0, 30) + "..."
+  const data = await mainContract.getData()
+  document.getElementById("counter")!.innerHTML = data.number.toString()
+  const balance = await mainContract.getBalance()
+  document.getElementById("balance")!.innerHTML = balance.balance.toString()
+})
 
 const tonConnectUI = new TonConnectUI({
   manifestUrl: "https://zavx0z.github.io/bun-ton/tonconnect-manifest.json",
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite"
+import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 export default defineConfig({
   base: "/bun-ton/",
+  plugins: [nodePolyfills()],
 })
